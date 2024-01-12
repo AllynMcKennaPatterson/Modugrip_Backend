8a52b72a0841f6a3715ec6f9de3d514ecd7cef5c
Allyn McKenna Patterson <allyncodes@gmail.com>
2024-01-12 09:59:29 +0000

    Initial commit

---
+++ src/main/java/ie/atu/modugrip_backend/ModugripBackendApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.modugrip_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModugripBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModugripBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ie/atu/modugrip_backend/ModugripBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.modugrip_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModugripBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
