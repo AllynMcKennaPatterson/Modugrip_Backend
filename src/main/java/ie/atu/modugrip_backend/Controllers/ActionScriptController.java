f780914b01ccd6b36c796d0f15c944fbd26daed4
G00392765@atu.ie <G00392765@atu.ie>
2024-01-22 10:47:31 +0000

    Created Publish Controller with postmapping to publish slider data to Ubidots

--- src/main/java/ie/atu/modugrip_backend/Controllers/ActionScriptController.java
+++
@@ -1,4 +0,0 @@
-package ie.atu.modugrip_backend.Controllers;
-
-public class ActionScriptController {
-}
---
+++ src/main/java/ie/atu/modugrip_backend/Controllers/PublishController.java
@@ -0,0 +1,24 @@
+package ie.atu.modugrip_backend.Controllers;
+
+import ie.atu.modugrip_backend.Models.SliderData;
+import ie.atu.modugrip_backend.Services.SliderDataService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PublishController {
+    SliderDataService sliderDataService;
+
+    public PublishController(SliderDataService sliderDataService){
+        this.sliderDataService = sliderDataService;
+    }
+
+    @PostMapping("/publish-slider")
+    public void publishSlider(@RequestBody SliderData sliderData) {
+        System.out.println("Publishing Slider data: " + sliderData);
+
+        sliderDataService.publishSliderData(sliderData);
+    }
+
+}
