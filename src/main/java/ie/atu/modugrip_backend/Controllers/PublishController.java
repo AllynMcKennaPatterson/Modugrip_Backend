31ad7eea5233e7152f2e756a2d3cebaf35124312
G00392765@atu.ie <G00392765@atu.ie>
2024-01-25 09:53:47 +0000

    Added publish-coordinates URI

--- src/main/java/ie/atu/modugrip_backend/Controllers/PublishController.java
+++ src/main/java/ie/atu/modugrip_backend/Controllers/PublishController.java
@@ -1,6 +1,8 @@
 package ie.atu.modugrip_backend.Controllers;
 
+import ie.atu.modugrip_backend.Models.Coordinates;
 import ie.atu.modugrip_backend.Models.SliderData;
+import ie.atu.modugrip_backend.Services.CoordinateService;
 import ie.atu.modugrip_backend.Services.SliderDataService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,9 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PublishController {
     SliderDataService sliderDataService;
+    CoordinateService coordinateService;
 
-    public PublishController(SliderDataService sliderDataService){
+    public PublishController(SliderDataService sliderDataService, CoordinateService coordinateService){
         this.sliderDataService = sliderDataService;
+        this.coordinateService = coordinateService;
     }
 
     @PostMapping("/publish-slider")
@@ -19,6 +23,13 @@ public class PublishController {
         System.out.println("Publishing Slider data: " + sliderData);
         sliderDataService.publishSliderData(sliderData);
         return sliderData;
+    }
+
+    @PostMapping("/publish-coordinates")
+    public Coordinates publishCoordinates(@RequestBody Coordinates coordinates) {
+        System.out.println("Publishing Coordinate data: " + coordinates);
+        coordinateService.publishCoordinateValues(coordinates);
+        return coordinates;
     }
 
 }
