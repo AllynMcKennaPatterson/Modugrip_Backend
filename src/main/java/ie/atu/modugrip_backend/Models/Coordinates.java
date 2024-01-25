dd1bc8015418e1a3697f3c57b36e6ead23b7dae8
G00392765@atu.ie <G00392765@atu.ie>
2024-01-25 09:53:08 +0000

    Created coordinates class

---
+++ src/main/java/ie/atu/modugrip_backend/Models/Coordinates.java
@@ -0,0 +1,14 @@
+package ie.atu.modugrip_backend.Models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Coordinates {
+    int x;
+    int y;
+    int z;
+}
