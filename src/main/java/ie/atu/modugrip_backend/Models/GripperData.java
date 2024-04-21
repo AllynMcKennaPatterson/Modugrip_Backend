d6ffb0c1a5e657d679af6321e872dc90172b344e
Allyn McKenna Patterson <allyncodes@gmail.com>
2024-04-21 22:34:56 +0100

    Created new model class

---
+++ src/main/java/ie/atu/modugrip_backend/Models/GripperData.java
@@ -0,0 +1,12 @@
+package ie.atu.modugrip_backend.Models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GripperData {
+    int servo4;
+}
---
+++ src/main/java/ie/atu/modugrip_backend/Models/ToolStatus.java
@@ -0,0 +1,13 @@
+package ie.atu.modugrip_backend.Models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ToolStatus {
+    int toolstatus;
+
+}
